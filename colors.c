e638ce6525ceee82530923ae52042268da284084
Marine Bascunana <marine.bascunana@gmail.com>
2022-03-14 23:09:31 +0100

    modif des couleurs

--- colors.c
+++ colors.c
@@ -39,35 +39,35 @@ void	ft_setup_colors(t_mlx *mlx)
 double		ft_lint(double start, double end, double percent)
 {
 	double color;
-    color = 0.0;
-    if (start == end)
+  color = 0.0;
+  if (start == end)
 		return (start);
-    color = start * (1.0 - percent) + (end * percent);
+  color = start * (1.0 - percent) + (end * percent);
 	return (color);
 }
 
 int	ft_colors(t_mlx *mlx)
 {
     double	divisor;
-	double	percent;
+	  double	percent;
     int color_start;
     color_start = 0;
     mlx->data.z = mlx->data.tab[mlx->data.y][mlx->data.x];
     divisor = mlx->color.z_max - mlx->color.z_min;
 
     // mlx->color.c_min = 0x3A3042;
-    mlx->color.c_min = 0xFFA69E;
-    mlx->color.c_third = 0xAED9E0;
-    mlx->color.c_max = 0xB8F2E6;
+    // mlx->color.c_min = 0xFFA69E;
+    // mlx->color.c_third = 0xAED9E0;
+    // mlx->color.c_max = 0xB8F2E6;
     // mlx->color.c_max = 0xFAF3DD;
-
-    if (mlx->color.next > mlx->data.z)
+    // ft_change_color(key, mlx);
+  if (mlx->color.next > mlx->data.z)
 		percent = (mlx->color.next - mlx->color.z_min) / divisor;
 	else if (divisor != 0)
 		percent = (mlx->data.z - mlx->color.z_min) / divisor;
 	else
 		percent = 0.0;
-    
+
 
 	if (percent == 0.0)
 		color_start = mlx->color.c_min;
@@ -80,21 +80,21 @@ int	ft_colors(t_mlx *mlx)
 	else if (percent == 1.0)
 		color_start = mlx->color.c_max;
 
-    
-    mlx->color.red = (int)ft_lint(((color_start >> 16) & 0xFF), ((mlx->color.c_max >> 16) & 0xFF), percent);
+
+  mlx->color.red = (int)ft_lint(((color_start >> 16) & 0xFF), ((mlx->color.c_max >> 16) & 0xFF), percent);
 	mlx->color.green = (int)ft_lint(((color_start >> 8) & 0xFF), ((mlx->color.c_max >> 8) & 0xFF), percent);
 	mlx->color.blue = (int)ft_lint((color_start & 0xFF), (mlx->color.c_max & 0xFF), percent);
 	return (mlx->color.red << 16 | mlx->color.green << 8 | mlx->color.blue);
 }
 
-void	ft_change_color(int key, t_mlx *mlx)
-{
-	if (key == 114)
-		mlx->color.red += 20;
-	else if (key == 103)
-		mlx->color.green += 20;
-	else if (key == 98)
-		mlx->color.blue += 20;
-	if (mlx->img.img_ptr)
-		mlx_destroy_image(mlx->mlx_ptr, mlx->img.img_ptr);
-}
+// void	ft_change_color(int key, t_mlx *mlx)
+// {
+// 	if (key == 114)
+// 		mlx->color.red += 20;
+// 	else if (key == 103)
+// 		mlx->color.green += 20;
+// 	else if (key == 98)
+// 		mlx->color.blue += 20;
+// 	if (mlx->img.img_ptr)
+// 		mlx_destroy_image(mlx->mlx_ptr, mlx->img.img_ptr);
+// }
--- event.c
+++ event.c
@@ -14,9 +14,9 @@
 
 void	ft_altitude(int key, t_mlx *mlx)
 {
-	if (key == 65451)
+	if (key == 65451 || key == 24)
 		mlx->altitude += 2;
-	else if (key == 65421)
+	else if (key == 65421 || key == 27)
 		mlx->altitude -= 2;
 	if (mlx->img.img_ptr)
 		mlx_destroy_image(mlx->mlx_ptr, mlx->img.img_ptr);
@@ -24,9 +24,9 @@ void	ft_altitude(int key, t_mlx *mlx)
 
 void	ft_zoom(int key, t_mlx *mlx)
 {
-	if (key == 112)
+	if (key == 112 || key == 35)
 		mlx->map.space += 5;
-	else if (key == 111)
+	else if (key == 111 || key == 31)
 		if (mlx->map.space > 5)
 			mlx->map.space -= 5;
 	if (mlx->img.img_ptr)
@@ -36,13 +36,13 @@ void	ft_zoom(int key, t_mlx *mlx)
 
 void	ft_move(int key, t_mlx *mlx)
 {
-	if (key == 65362)
+	if (key == 65362 || key == 126)
 		mlx->pad_top_bot -= 40;
-	else if (key == 65364)
+	else if (key == 65364 || key == 125)
 		mlx->pad_top_bot += 40;
-	else if (key == 65361)
+	else if (key == 65361 || key == 123)
 		mlx->pad_left_right -= 40;
-	else if (key == 65363)
+	else if (key == 6536 || key == 124)
 		mlx->pad_left_right += 40;
 	if (mlx->img.img_ptr)
 		mlx_destroy_image(mlx->mlx_ptr, mlx->img.img_ptr);
@@ -50,39 +50,57 @@ void	ft_move(int key, t_mlx *mlx)
 
 void	ft_set_angle(int key, t_mlx *mlx)
 {
-	if (key == 107)
+	if (key == 47)
 		mlx->x_angle -= 5;
-	else if (key == 106)
+	else if (key == 43)
 		mlx->x_angle += 5;
-	
-	// printf("angle : %f\n", mlx->x_angle);
+
+	printf("angle : %f\n", mlx->x_angle);
 	// else if (key == 6)
 	// 	map_info->controls->x_angle -= 5;
 	// else if (key == 7)
 	// 	map_info->controls->x_angle += 5;
 	// mlx_clear_window(MLX, WIN);
 	// expose_hook(map_info);
+  mlx_destroy_image(mlx->mlx_ptr, mlx->img.img_ptr);
+}
+
+void  ft_change_color(int key, t_mlx *mlx)
+{
+  if (key == 12)
+  {
+    mlx->color.c_min = 0xFFA69E;
+    mlx->color.c_max = 0xB8F2E6;
+  }
+  else if (key == 13)
+  {
+    mlx->color.c_min = 0x3A3042;
+    mlx->color.c_max = 0xFFA69E;
+  }
+  mlx_destroy_image(mlx->mlx_ptr, mlx->img.img_ptr);
 }
 
 //appelee avec numero de touche et void * transmis dans le main
 //utiliser un putnbr pour recuperer le numero dela touche et adapter comportement en fonction de la touche
 int deal_key(int key, t_mlx *mlx)
-{ 
-	// printf("key = %d\n", key);
+{
+	printf("key = %d\n", key);
 	if (key == 65307)
 		ft_exit_win(mlx);
-	else if (key == 65451 || key == 65421)
+	else if (key == 65451 || key == 65421 || key == 24 || key == 27)
 		ft_altitude(key, mlx);
-	else if (key == 112 || key == 111)
+	else if (key == 112 || key == 111 || key == 35 || key == 31)
 		ft_zoom(key, mlx);
-	else if (key == 65362 || key == 65364 || key == 65363 || key == 65361)
+	else if (key == 65362 || key == 65364 || key == 65363 || key == 65361 || key == 123 || key == 124 || key == 125 || key == 126)
 		ft_move(key, mlx);
 	else if (key == 114 || key == 103 || key == 98)
 		ft_change_color(key, mlx);
-	else if (key == 107|| key == 106)
+	else if (key == 47|| key == 43)
 		ft_set_angle(key, mlx);
-	ft_print_window(mlx);
-	return (0);
+  else if (key == 12 || key == 13)
+    ft_change_color(key, mlx);
+  ft_print_window(mlx);
+  return (0);
 }
 // int     mlx_mouse_move(void *win_ptr, int x, int y);
 void	ft_move_mouse(int key, t_mlx *mlx)
